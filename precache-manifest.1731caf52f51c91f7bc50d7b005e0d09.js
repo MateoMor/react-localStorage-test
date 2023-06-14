9953cd8883f4a262e59fe67f0a27c999abae7e29
Mateo <frisded555@gmail.com>
2023-06-13 19:44:56 -0500

    Updates

---
+++ precache-manifest.1731caf52f51c91f7bc50d7b005e0d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ed3c2e4a3bf6c4f52ec",
+    "url": "/static/css/main.601d2d35.chunk.css"
+  },
+  {
+    "revision": "4ed3c2e4a3bf6c4f52ec",
+    "url": "/static/js/main.4ed3c2e4.chunk.js"
+  },
+  {
+    "revision": "4d369398ab6836e5dd78",
+    "url": "/static/css/1.2318de80.chunk.css"
+  },
+  {
+    "revision": "4d369398ab6836e5dd78",
+    "url": "/static/js/1.4d369398.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64b5a7719ca24ad40de1c9fffb841ef8",
+    "url": "/index.html"
+  }
+];
