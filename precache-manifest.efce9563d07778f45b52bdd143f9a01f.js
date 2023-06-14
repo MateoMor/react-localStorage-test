c993d5e7b5ca08becb6c8e0978a6c990af2461c2
Mateo <frisded555@gmail.com>
2023-06-13 19:49:18 -0500

    Updates

---
+++ precache-manifest.efce9563d07778f45b52bdd143f9a01f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ed3c2e4a3bf6c4f52ec",
+    "url": "/react-localStorage-test/static/css/main.601d2d35.chunk.css"
+  },
+  {
+    "revision": "4ed3c2e4a3bf6c4f52ec",
+    "url": "/react-localStorage-test/static/js/main.4ed3c2e4.chunk.js"
+  },
+  {
+    "revision": "4d369398ab6836e5dd78",
+    "url": "/react-localStorage-test/static/css/1.2318de80.chunk.css"
+  },
+  {
+    "revision": "4d369398ab6836e5dd78",
+    "url": "/react-localStorage-test/static/js/1.4d369398.chunk.js"
+  },
+  {
+    "revision": "e4a8bd9c451b1adce733",
+    "url": "/react-localStorage-test/static/js/runtime~main.e4a8bd9c.js"
+  },
+  {
+    "revision": "9c22a05843fc3f5ab8c5d7b33e27081e",
+    "url": "/react-localStorage-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1731caf52f51c91f7bc50d7b005e0d09.js"
+  "/react-localStorage-test/precache-manifest.efce9563d07778f45b52bdd143f9a01f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-localStorage-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
